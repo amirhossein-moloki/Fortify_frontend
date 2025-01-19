124781c25873ae84ad5e2ea0baf9f7713ddd979a
amirhossein-moloki <amirh.moloki@gmail.com>
2025-01-19 21:14:45 +0330

    Update next.config.ts URLS

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    BASE_URL: 'http://localhost:8000/',
-    BASE_URL_MD: 'http://localhost:8000', 
+    BASE_URL: 'https://fortify-c8os.onrender.com/:8000/',
+    BASE_URL_MD: 'https://fortify-c8os.onrender.com/:8000', 
   },
   // سایر تنظیمات (در صورت نیاز)
   reactStrictMode: true, // این مورد اختیاری است و به‌طور پیش‌فرض فعال است
