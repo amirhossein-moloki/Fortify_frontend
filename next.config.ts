393e864b8ae9bd0e79c5617e06b0793eb94acc40
amir <amir.moloki8558@gmail.com>
2025-01-22 14:15:49 +0330

    add audio files

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    BASE_URL: 'https://fortify-c8os.onrender.com/:8000/',
-    BASE_URL_MD: 'https://fortify-c8os.onrender.com/:8000', 
+    BASE_URL: 'http://fortify-c8os.onrender.com:8000/',
+    BASE_URL_MD: 'http://fortify-c8os.onrender.com:8000',
   },
-  // سایر تنظیمات (در صورت نیاز)
-  reactStrictMode: true, // این مورد اختیاری است و به‌طور پیش‌فرض فعال است
-  
-  // تنظیم مسیر خروجی برای build
-  distDir: 'build', // مسیر خروجی را به 'build' تغییر می‌دهیم
+  reactStrictMode: true,
+  distDir: 'build',
 };
 
 export default nextConfig;
