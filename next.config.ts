f6013a00bb68c3cb017a39c65f772e37c90fe9dd
amir <amir.moloki8558@gmail.com>
2025-01-22 15:21:17 +0330

    update base url

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    BASE_URL: 'http://fortify-c8os.onrender.com:8000/',
-    BASE_URL_MD: 'http://fortify-c8os.onrender.com:8000',
+    BASE_URL: 'https://fortify-c8os.onrender.com:8000/',
+    BASE_URL_MD: 'https://fortify-c8os.onrender.:8000/',
   },
   reactStrictMode: true,
 };
