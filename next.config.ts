39958d36f8d91acf9926e85d5c6d2bd1209f03f9
Amir <amir.moloki8558@gmail.com>
2025-02-13 20:55:15 +0330

    update project for publish

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    BASE_URL: 'https://fortify-c8os.onrender.com/',
-    BASE_URL_MD: 'https://fortify-c8os.onrender.com/',
+    BASE_URL: 'https://your-production-url.com/',
+    BASE_URL_MD: 'https://your-production-url.com/',
   },
   reactStrictMode: true,
   swcMinify: true,
